25b1ed5b8fc7cbcb41e1b6e54e2ab214cdd92dbf
Benjamin Avila. <36257924+BenjaminAR@users.noreply.github.com>
2026-04-21 12:04:38 -0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        Producto p1 = new ProductoPrecioFijo("pr1", "Coca-cola", 0.16, 19.5);
+  
+        Producto p2 = new ProductoPesoVariable("pr2", "Mango", 0.00, 1, 40.0, "Kg");
+        
+        ProductoCompuesto despensa = new ProductoCompuesto("Cb001", "Canasta básica",  0.0);
+        
+        despensa.agregarProducto(p1);
+        despensa.agregarProducto(p2);
+        
+        System.out.println("P1:" + p1.getValorVenta());
+        System.out.println("TOTAL DESPENSA: $" + despensa.getValorVenta());
+        
+    }
+}
